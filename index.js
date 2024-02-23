f2173d37ac2d73f11c13b57ab2e1fd1d0e08811b
Kiyo.io <tishaya.thompson@outlook.com>
2024-02-22 19:57:08 -0500

    organizing js for readability

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-alert("Hello World!");
-
-let c = document.getElementById("canvas");
-let ctx = c.getContext("2d");
-let tx = window.innerWidth;
-let ty = window.innerHeight;
-canvas.width = tx;
-canvas.height = ty;
-//c.lineWidth= 5;
-//c.globalAlpha = 0.5;
-
-let mousex = 0;
-let mousey = 0;
-
 // Filled TEXT Writing
 // ctx.font = "30px Arial";
 // ctx.fillText("Hello World", 10, 40);
@@ -32,7 +18,7 @@ let mousey = 0;
 
 // Draw Linear GADIENT
 // Create gradient
-// var grd = ctx.createLinearGradient(0, 0, 200, 0);
+// let grd = ctx.createLinearGradient(0, 0, 200, 0);
 // grd.addColorStop(0, "red");
 // grd.addColorStop(1, "white");
 
@@ -42,7 +28,7 @@ let mousey = 0;
 
 // Draw Circular GRADIENT
 // Create gradient
-// var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+// let grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
 // grd.addColorStop(0, "red");
 // grd.addColorStop(1, "white");
 
@@ -50,14 +36,30 @@ let mousey = 0;
 // ctx.fillStyle = grd;
 // ctx.fillRect(10, 10, 150, 80);
 
+console.log("Hello World");
+
+let canvas = document.getElementById("canvas");
+let c = canvas.getContext("2d");
+let tx = window.innerWidth;
+let ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
+//c.lineWidth= 5;
+//c.globalAlpha = 0.5;
+
+// Mouse Tracking
+let mousex = 0;
+let mousey = 0;
+
 addEventListener("mousemove", function () {
   mousex = event.clientX;
   mousey = event.clientY;
 });
 
 let grav = 0.99;
-c.strokeWidth = 5;
+c.lineWidth = 5;
 
+// Generates random RGB color
 function randomColor() {
   return (
     "rgba(" +
@@ -71,7 +73,7 @@ function randomColor() {
     ")"
   );
 }
-// Creates random sized balls
+// Draws new ball with random color, size, and velocity
 function Ball() {
   this.color = randomColor();
   this.radius = Math.random() * 20 + 14;
@@ -89,8 +91,55 @@ function Ball() {
     //c.stroke();
   };
 }
-// Array  and for loop that creates a new random ball created by Ball()
+
+// Makes 50 new random balls using ball() and pushes to an array
 let bal = [];
 for (let i = 0; i < 50; i++) {
   bal.push(new Ball());
 }
+
+// updates and renders the animation continously
+function animate() {
+  if (tx != window.innerWidth || ty != window.innerHeight) {
+    tx = window.innerWidth;
+    ty = window.innerHeight;
+    canvas.width = tx;
+    canvas.height = ty;
+  }
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, tx, ty);
+
+  // i will increase and update each loop the loop is only as long as the bal array so it will cycle through each object in the array
+  for (let i = 0; i < bal.length; i++) {
+    bal[i].update();
+    bal[i].y += bal[i].dy;
+    bal[i].x += bal[i].dx;
+    if (bal[i].y + bal[i].radius >= ty) {
+      bal[i].dy = -bal[i].dy * grav;
+    } else {
+      bal[i].dy += bal[i].vel;
+    }
+    if (bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0) {
+      bal[i].dx = -bal[i].dx;
+    }
+    if (
+      mousex > bal[i].x - 20 &&
+      mousex < bal[i].x + 20 &&
+      mousey > bal[i].y - 50 &&
+      mousey < bal[i].y + 50 &&
+      bal[i].radius < 70
+    ) {
+      //bal[i].x += +1;
+      bal[i].radius += 5;
+    } else {
+      if (bal[i].radius > bal[i].startradius) {
+        bal[i].radius += -5;
+      }
+    }
+
+    //forloop end
+  }
+  //animation end
+}
+
+animate();
